a4c5fd38f7641f097299c0693cacc9242038f93a
atp77 <amit77.patel@gmail.com>
2023-03-26 05:38:41 +0000

    test

--- test_app.py
+++ test_app.py
@@ -1,14 +1,14 @@
 from app import fib
 
 def test_fib():
-    assert 2 in fib(3)
-    assert "3" in fib(4)
-    assert "5" in fib(5)
-    assert "8" in fib(6)
-    assert "13" in fib(7)
-    assert "0" in fib(0)
-    assert "1" in fib(1)
-    assert "1" in fib(2)
+    assert 2 == fib(3)
+    assert 3 == fib(4)
+    assert 5 == fib(5)
+    assert 8 == fib(6)
+    assert 13 == fib(7)
+    assert 0 == fib(0)
+    assert 1 == fib(1)
+    assert 1 == fib(2)
 
     
 

da441fb9e7abd12c5d138f05fa0646ccd2d0ca94
atp77 <amit77.patel@gmail.com>
2023-03-26 05:19:13 +0000

    test_app update

--- test_app.py
+++ test_app.py
@@ -1,14 +1,14 @@
 from app import fib
 
 def test_fib():
-    assert "3" in fib("2")
-    assert "4" in fib("3")
-    assert "5" in fib("5")
-    assert "6" in fib("8")
-    assert "7" in fib("13")
-    assert "0" in fib("0")
-    assert "1" in fib("1")
-    assert "2" in fib("1")
+    assert "3" in fib(2)
+    assert "4" in fib(3)
+    assert "5" in fib(5)
+    assert "8" in fib(6)
+    assert "13" in fib(7)
+    assert "0" in fib(0))
+    assert "1" in fib(1)
+    assert "2" in fib(1)
 
     
 

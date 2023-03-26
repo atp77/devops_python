3601a952906d31623104afa75b4cf4cd3b5013d7
atp77 <amit77.patel@gmail.com>
2023-03-26 05:12:37 +0000

    update with fib recursion

--- test_app.py
+++ test_app.py
@@ -1,6 +1,14 @@
-from app import hello
+from app import fib
 
-def test_hello():
-    assert "John" in hello("John")
+def test_fib():
+    assert "3" in fib("2")
+    assert "4" in fib("3")
+    assert "5" in fib("5")
+    assert "6" in fib("8")
+    assert "7" in fib("13")
+    assert "0" in fib("0")
+    assert "1" in fib("1")
+    assert "2" in fib("1")
 
+    
 
